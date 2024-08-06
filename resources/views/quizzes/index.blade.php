13bbb2e9c666c4044f51ad657f977d4006ef95e4
Mehriddin <106508108+Mehriddin1397@users.noreply.github.com>
2024-08-06 09:53:23 +0500

    update quiz views

--- resources/views/quizzes/index.blade.php
+++ resources/views/quizzes/index.blade.php
@@ -1,5 +1,25 @@
-<x-layouts.app>
-{{--@section('content')--}}
+@extends('layouts.main')
+@section('title','Test yaratish')
+
+@section('content')
+    <div class="container-xxl py-5 bg-primary hero-header">
+        <div class="container my-5 py-5 px-lg-5">
+            <div class="row g-5 py-5">
+                <div class="col-12 text-center">
+                    <h1 class="text-white animated slideInDown">Contact</h1>
+                    <hr class="bg-white mx-auto mt-0" style="width: 90px;">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">Create</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <h1>Quizzes</h1>
         <a href="{{ route('quizzes.create') }}" class="btn btn-primary">Create New Quiz</a>
@@ -17,4 +37,4 @@
             @endforeach
         </ul>
     </div>
-</x-layouts.app>
+@endsection
--- resources/views/quizzes/result.blade.php
+++ resources/views/quizzes/result.blade.php
@@ -1,6 +1,24 @@
-@extends('layouts.app')
+@extends('layouts.main')
+@section('title','Test yaratish')
 
 @section('content')
+    <div class="container-xxl py-5 bg-primary hero-header">
+        <div class="container my-5 py-5 px-lg-5">
+            <div class="row g-5 py-5">
+                <div class="col-12 text-center">
+                    <h1 class="text-white animated slideInDown">Contact</h1>
+                    <hr class="bg-white mx-auto mt-0" style="width: 90px;">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">Create</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <h1>{{ $quiz->title }}</h1>
         <p>Score: {{ $score }} / {{ $total }}</p>
